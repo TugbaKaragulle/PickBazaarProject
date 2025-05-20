dd040fc28c75670693f1703b3d36af5da2bc15c9
sengul-coder <sengulturhan2015@gmail.com>
2025-05-20 09:02:09 +0100

    Login page updated

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -18,7 +18,7 @@ public class LoginPageTest {
         Driver.getDriver().get(ConfigReader.getProperty("pickbazar_url"));
 
         AllPages allPages = new AllPages();
-        SoftAssert softAssert= new SoftAssert();
+        SoftAssert softAssert = new SoftAssert();
 
         allPages.loginPage().logIn(ConfigReader.getProperty("loginEmail"), ConfigReader.getProperty("loginPassword"));
         softAssert.assertTrue(allPages.loginPage().isProfileImageDisplayed());
@@ -28,22 +28,89 @@ public class LoginPageTest {
         Driver.closeDriver();
 
     }
-@Test
-    public void TC_017_02 (ITestContext context) throws InterruptedException {
-    setupBrowser(context);
-    Driver.getDriver().get(ConfigReader.getProperty("pickbazar_url"));
 
-    AllPages allPages = new AllPages();
-    SoftAssert softAssert= new SoftAssert();
-    allPages.pickBazarHomePage().clickJoinButton();
-    allPages.loginPage().forgotPasswordClick();
+    @Test
+    public void TC_017_02(ITestContext context) throws InterruptedException {
+        setupBrowser(context);
+        Driver.getDriver().get(ConfigReader.getProperty("pickbazar_url"));
+
+        AllPages allPages = new AllPages();
+        SoftAssert softAssert = new SoftAssert();
+
+        allPages.pickBazarHomePage().clickJoinButton();
+        allPages.loginPage().forgotPasswordClick();
+        allPages.loginPage().sendEmail();
+        allPages.loginPage().submitEmailClick();
+        softAssert.assertTrue(allPages.loginPage().passwordChangeSuccessfully());
+        softAssert.assertAll();
+        Driver.closeDriver();
 
 
-}
+    }
+
+    @Test
+    public void TC_017_03(ITestContext context) {
+        setupBrowser(context);
+        Driver.getDriver().get(ConfigReader.getProperty("pickbazar_url"));
+
+        AllPages allPages = new AllPages();
+        SoftAssert softAssert = new SoftAssert();
+        allPages.pickBazarHomePage().clickJoinButton();
+        allPages.loginPage().sendPassword();
+        allPages.loginPage().isEyeImageDisplayed();
+        softAssert.assertTrue(allPages.loginPage().isEyeImageDisplayed());
+        softAssert.assertAll();
+
+
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void TC_017_04(ITestContext context) {
+        setupBrowser(context);
+        Driver.getDriver().get(ConfigReader.getProperty("pickbazar_url"));
+
+        AllPages allPages = new AllPages();
+        SoftAssert softAssert = new SoftAssert();
+        allPages.pickBazarHomePage().clickJoinButton();
+        allPages.loginPage().googleLoginClick();
+        softAssert.assertFalse(allPages.loginPage().googleErroorMessageIsdisplayed(), "Login olunamadi");
+        softAssert.assertAll();
+        Driver.closeDriver();
+
+    }
+    @Test
+    public void TC_017_05(ITestContext context){
+        setupBrowser(context);
+        Driver.getDriver().get(ConfigReader.getProperty("pickbazar_url"));
+
+        AllPages allPages = new AllPages();
+        SoftAssert softAssert = new SoftAssert();
+        allPages.pickBazarHomePage().clickJoinButton();
+        allPages.loginPage().loginWithMobileButtonClick();
+        allPages.loginPage().sendTelNumber();
+        allPages.loginPage().sendOPTClick();
+        allPages.loginPage().sendOPTCode();
+        allPages.loginPage().verifyCodeButtonClick();
+        softAssert.assertTrue(allPages.loginPage().isProfileImageDisplayed());
+        softAssert.assertAll();
+
+
+        Driver.closeDriver();
+
+
+
+
 
     }
 
 
+
+
+
+
+
+}
 
 
 
