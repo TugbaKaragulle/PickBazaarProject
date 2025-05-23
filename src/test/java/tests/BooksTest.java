5e795dc45778f32079f88cd19047e99b41715f12
sengul-coder <sengulturhan2015@gmail.com>
2025-05-23 10:19:52 +0100

    conflict resolved

--- src/test/java/tests/BooksTest.java
+++ src/test/java/tests/BooksTest.java
@@ -1,234 +1,233 @@
 package tests;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import io.qameta.allure.*;
 import org.testng.ITestContext;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.AllPages;
-import utilities.ConfigReader;
 import utilities.Driver;
-import utilities.ReusableMethods;
-import static utilities.Driver.getDriver;
 import static utilities.Driver.setupBrowser;
 //***
 
 public class BooksTest {
 
-    Logger logger = LogManager.getLogger(BooksTest.class);
-
-    @Test(groups = "smoke")
+    @Test(groups = "regression")
+    @Owner("Tugba")
+    @Description("TC_23_01_Books Page-Banner test")
+    @Severity(SeverityLevel.NORMAL)
     public void TC_23_01(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Verifying that the banner with 'Sale UP TO 20% OFF' and 'Shop Now' is visible.");
-        softAssert.assertTrue(allPages.booksPage().isBannerDisplayed(),"Banner with 'Sale UP TO 20% OFF' is not visible.");
+
+        Allure.step("Verifying that the banner with 'Sale UP TO 20% OFF' and 'Shop Now' is visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().isBannerDisplayed(), "Banner with 'Sale UP TO 20% OFF' is not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    @Test(groups = "smoke") //TODO ----> BUG
+    @Test(groups = {"regression","smoke"})
+    @Owner("Tugba")
+    @Description("TC_23_02_Books Page – Test of the \"Best Selling Products\" section")
+    @Severity(SeverityLevel.NORMAL)
     public void TC_23_02(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        String title ="Best Selling Products";
-        logger.info("Verifying that the 'Best Selling Products' section is visible.");
-        softAssert.assertTrue(allPages.booksPage().bestSellingProductsField(title),"'Best Selling Products' section is not visible.");
+
+        Allure.step("Verifying that the 'Best Selling Products' section is visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().bestSellingProductsField(allPages.booksPage().getBestSellingtitle()),
+                    "'Best Selling Products' section is not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    @Test(groups = "smoke")
+    @Test(groups = {"regression","smoke"})
+    @Owner("Tugba")
+    @Description("TC_023_03_Books Page – Test of the \"Popular Products\" section")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC_23_03(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'Popular Products' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getPopularProductTitle());
-        logger.info("Verifying that product images are visible.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getPopularProductImages()),"Product images are not visible");
+
+        Allure.step("Verifying that product images are visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getPopularProductImages(),
+                    allPages.booksPage().getPopularProductTitle(), "product images"),"Product images are not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    @Test(groups = "smoke")
+    @Test(groups = {"regression","smoke"})
+    @Owner("Tugba")
+    @Description("TC_023_04_Books Page – Test of the \"Popular Products\" section")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC_23_04(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'Popular Products' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getPopularProductTitle());
-        logger.info("Verifying that product names are visible.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getPopularProductBookNames()),"Product names are visible.");
+
+        Allure.step("Verifying that product names are visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getPopularProductBookNames(),
+                    allPages.booksPage().getPopularProductTitle(), "product names"), "Product names are not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    @Test(groups = "smoke")
+    @Test(groups = {"regression","smoke"})
+    @Owner("Tugba")
+    @Description("TC_023_05_Books Page – Test of the \"Popular Products\" section")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC_23_05(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'Popular Products' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getPopularProductTitle());
-        logger.info("Verifying that product prices are visible.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getPopularPruductPrice()),"Product prices are visible");
+
+        Allure.step("Verifying that product prices are visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getPopularPruductPrice(),
+                    allPages.booksPage().getPopularProductTitle(), "product prices"),"Product prices are not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-
-    @Test(groups = "smoke")
+    @Test(groups = "regression")
+    @Owner("Tugba")
+    @Description("TC_023_06_Books Page – Test of the \"Which Books You Like To See?\" section")
+    @Severity(SeverityLevel.NORMAL)
     public void TC_23_06(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'Which Books You Like To See?' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getWhichBookTitle());
-        logger.info("Verifying that the section title is visible.");
-        softAssert.assertTrue(ReusableMethods.isWebElementDisplayed(allPages.booksPage().getWhichBookTitle()),"'Which Books You Like To See?' title is not visible.");
+
+        Allure.step("Verifying that the section title is visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().isTitleVisible(allPages.booksPage().getWhichBookTitle()),
+                    "'Which Books You Like To See?' title is not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    @Test(groups = "smoke")
+    @Test(groups = {"regression","smoke"})
+    @Owner("Tugba")
+    @Description("TC_23_07_Books Page – Test of the \"Which Books You Like To See?\" section")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC_23_07(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'Which Books You Like To See?' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getWhichBookTitle());
-        logger.info("Clicking on the scroll arrow.");
-        allPages.booksPage().clickAsMuchAsYouWant(allPages.booksPage().getWhichBookScrollArrow(),1);
-        logger.info("Verifying that the book images are displayed.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getWhichBookImages()),"Book images are not visible.");
+
+        Allure.step("Verifying that the book images are displayed.", () -> {
+            softAssert.assertTrue(allPages.booksPage().isWhichBookSectionDisplayed(allPages.booksPage().getWhichBookImages(),
+                    allPages.booksPage().getWhichBookTitle(), "images"), "Book images are not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    //*********************************************
-
-
-    @Test(groups = "smoke")
+    @Test(groups = {"regression","smoke"})
+    @Owner("Tugba")
+    @Description("TC_023_08_Books Page – Visibility test of the titles under the \"Which Books You Like To See?\" section")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC_23_08(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'Which Books You Like To See?' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getWhichBookTitle());
-        logger.info("Clicking on the scroll arrow.");
-        allPages.booksPage().clickAsMuchAsYouWant(allPages.booksPage().getWhichBookScrollArrow(),1);
-        logger.info("Verifying that the book titles are displayed.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getWhichBookNames()),"Book titles are not visible.");
+
+        Allure.step("Verifying that the book titles are displayed.", () -> {
+            softAssert.assertTrue(allPages.booksPage().isWhichBookSectionDisplayed(allPages.booksPage().getWhichBookNames(),
+                    allPages.booksPage().getWhichBookTitle(), "names"), "Book names are not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
     //TODO---> 23_09 isimli @Test MANUEL BUG
 
-
-
-    @Test(groups = "smoke")
+    @Test(groups ={"regression","smoke"})
+    @Owner("Tugba")
+    @Description("TC_023_10_Books Page – Test of the \"New Arrival Books\" section")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC_23_10(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'New Arrival Books' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getNewArrivalTitle());
-        logger.info("Verifying that new book images are visible.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getNewArrivalBooksImg()),"New book images are not visible.");
+
+        Allure.step("Verifying that new book images are visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getNewArrivalBooksImg(),
+                    allPages.booksPage().getNewArrivalTitle(), "new book images"), "New book images are not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    @Test(groups = "smoke")
+    @Test(groups = {"regression","smoke"})
+    @Owner("Tugba")
+    @Description("TC_023_11 Books Page – Test of the \"New Arrival Books\" section")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC_23_11(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'New Arrival Books' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getNewArrivalTitle());
-        logger.info("Verifying that new book titles are visible.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getNewArrivalBookNames()), "New book titles are not visible.");
+
+        Allure.step("Verifying that new book titles are visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getNewArrivalBookNames(),
+                    allPages.booksPage().getNewArrivalTitle(), "new book titles"),"New book titles are not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    @Test(groups = "smoke")
+    @Test(groups = {"regression","smoke"})
+    @Owner("Tugba")
+    @Description("TC_023_12 Books Page – Test of the \"New Arrival Books\" section")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC_23_12(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'New Arrival Books' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getNewArrivalTitle());
-        logger.info("Verifying that new book prices are visible.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getNewArrivalBooksPrice()), "New book prices are not visible.");
+
+        Allure.step("Verifying that new book prices are visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getNewArrivalBooksPrice(),
+                    allPages.booksPage().getNewArrivalTitle(), "new book prices"), "New book prices are not visible."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    //**********************************************************************
-
-    @Test(groups = "smoke")
+    @Test(groups = "regression")
+    @Owner("Tugba")
+    @Description("TC_023_13_Books Page – Test of the \"Top Authors\" section")
+    @Severity(SeverityLevel.NORMAL)
     public void TC_23_13(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'Top Authors' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getTopAuthorsTitle());
-        logger.info("Clicking the scroll arrow 3 times.");
-        logger.info("Verifying that author photos are visible.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getFirst5AuthorPhotos()),"Author photos are not visible.");
-        allPages.booksPage().clickAsMuchAsYouWant(allPages.booksPage().getAuthorScrollArrowRight(),3);
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getLast5AuthorPhotos()),"Author photos are not visible.");
+
+        Allure.step("Verifying that author photos are visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().areAuthorPhotosDisplayed(),"Author photos are not visible.");});
 
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-    @Test(groups = "smoke")
+    @Test(groups = "regression")
+    @Owner("Tugba")
+    @Description("TC_023_14_Books Page – Test of the \"Top Authors\" section")
+    @Severity(SeverityLevel.NORMAL)
     public void TC_23_14(ITestContext context) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
         setupBrowser(context);
-        logger.info("Navigating to the homepage.");
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Scrolling to 'Top Authors' section.");
-        allPages.booksPage().scrollToElementOnBookPage(allPages.booksPage().getTopAuthorsTitle());
-        logger.info("Verifying that author names are visible.");
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getFirst5AuthorNames()),"Author names are not visible.");
-        allPages.booksPage().clickAsMuchAsYouWant(allPages.booksPage().getAuthorScrollArrowRight(),3);
-        softAssert.assertTrue(allPages.booksPage().areElementsDisplayed(allPages.booksPage().getLast5AuthorNames()),"Author names are not visible.");
+
+        Allure.step("Verifying that author names are visible.", () -> {
+            softAssert.assertTrue(allPages.booksPage().areAuthorNamesDisplayed(),"Author names are not visible.");});
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
-
-
-
 }
--- src/test/java/tests/LoginProfileTest.java
+++ src/test/java/tests/LoginProfileTest.java
@@ -8,9 +8,8 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.AllPages;
-import utilities.ConfigReader;
 import utilities.Driver;
-import static utilities.Driver.getDriver;
+
 import static utilities.Driver.setupBrowser;
 //***
 
@@ -18,35 +17,39 @@ public class LoginProfileTest {
     Logger logger = LogManager.getLogger(LoginProfileTest.class);
 
 
-    @Test(groups = "smoke")
+    @Test(groups = "regression")
     @Owner("Tugba")
-    @Description("Deneme Description")
+    @Description("User sees their points after login.")
     @Severity(SeverityLevel.CRITICAL)
     public void TC_021_01(ITestContext context) {
-
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
         setupBrowser(context);
-        logger.info("Customer sees their points on the profile page.");
-        softAssert.assertTrue(allPages.loginProfilePage().profilePoints(),"Customer cannot see their points on the profile page.");
+
+        Allure.step("Verifying that the user sees their points after login.", () -> {
+            softAssert.assertTrue(allPages.loginProfilePage().profilePoints(),
+                    "Customer cannot see their points on the profile page."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
 
-
-    @Test(dataProvider = "urlData", groups = "smoke")
-    public void TC_021(ITestContext context, String data, String testCaseNo){
-
+    @Test(dataProvider = "urlData", groups = {"regression", "smoke"})
+    @Owner("Tugba")
+    @Description("Test of opening the page after clicking each profile menu item.")
+    @Severity(SeverityLevel.CRITICAL)
+    public void TC_021_(ITestContext context, String data, String testCaseNo) {
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
-        getDriver().get(ConfigReader.getProperty("pickbazar_url"));
         setupBrowser(context);
-        logger.info("Executing test case: " + testCaseNo + " with data: " + data);
+
         try {
-            softAssert.assertTrue(allPages.loginProfilePage().profileDropDownMenu(data),"The page '" + data + "' could not be opened");
+            Allure.step("Verifying that clicking on the '" + data + "' menu item opens the correct page.", () -> {
+                softAssert.assertTrue(
+                        allPages.loginProfilePage().profileDropDownMenu(data),
+                        testCaseNo + " FAILED: The page '" + data + "' could not be opened"); });
         } catch (Exception e) {
-            softAssert.fail(testCaseNo + " is failed due to an "+e+" exception.");
+            softAssert.fail(testCaseNo + " is failed due to an " + e.getMessage() + " exception.");
         } finally {
             Driver.closeDriver();
             softAssert.assertAll();
@@ -55,22 +58,27 @@ public class LoginProfileTest {
 
     @DataProvider(name = "urlData")
     public Object[][] urlData() {
-        return new Object[][] {
-                { "profile", "TC_021_02" },
-                { "orders", "TC_021_03" },
-                { "wishlists", "TC_021_04" },
-                { "checkout", " TC_021_05" }
-        } ;
+        return new Object[][]{
+                {"profile", "TC_021_02"},
+                {"orders", "TC_021_03"},
+                {"wishlists", "TC_021_04"},
+                {"checkout", " TC_021_05"}
+        };
     }
 
-    @Test(groups = "smoke")
+    @Test(groups = {"regression", "smoke"})
+    @Owner("Tugba")
+    @Description("Test of logout")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC_021_06(ITestContext context) {
         setupBrowser(context);
         AllPages allPages = new AllPages();
         SoftAssert softAssert = new SoftAssert();
-        Driver.getDriver().get(ConfigReader.getProperty("pickbazar_url"));
-        logger.info("Checking if the 'Join' button is visible after clicking logout");
-        softAssert.assertTrue(allPages.loginProfilePage().verifyLogoutWorks(),"'Join' button is not visible after logging out.");
+
+        Allure.step("Verifying that the user is logged out and the 'Join' button is visible.", () -> {
+            softAssert.assertTrue(allPages.loginProfilePage().verifyLogoutWorks(),
+                    "'Join' button is not visible after logging out."); });
+
         softAssert.assertAll();
         Driver.closeDriver();
     }
